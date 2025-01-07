d5bd0684adcf479e2da72e01ddd15740364bdd09
Finn <finnbusse@outlook.de>
2025-01-07 17:03:51 +0100

    some color adjustments

--- src/sites/ChatPage.tsx
+++ src/sites/ChatPage.tsx
@@ -378,16 +378,14 @@ l32 -72 81 -31 c92 -35 178 -57 266 -66 56 -6 72 -2 235 54 96 34 175 61 177
                                 {exampleQuestions.map((question, index) => (
                                     <span
                                         key={index}
-                                        className="suggestion-box bg-white text-black border border-black rounded-xl py-4 px-5 text-base text-gray-800 shadow-md hover:bg-gray-100 cursor-pointer min-w-[150px] max-w-[200px] text-center overflow-hidden text-ellipsis whitespace-nowrap flex items-center justify-center dark:bg-gray-700 dark:text-white dark:border-none dark:hover:bg-gray-600"
+                                        className="suggestion-box border border-gray-400 bg-gray-50 text-wrap dark:text-white dark:bg-gray-700 rounded-xl py-4 px-5 text-base text-gray-800 shadow-md hover:bg-gray-200 cursor-pointer min-w-[150px] max-w-[200px] text-center overflow-hidden text-ellipsis whitespace-nowrap flex items-center justify-center"
                                         onClick={() => {
                                             setInputText(question);
                                             handleSend();
                                         }}
                                     >
-    {question}
-</span>
-
-
+                {question}
+            </span>
                                 ))}
                             </div>
 
