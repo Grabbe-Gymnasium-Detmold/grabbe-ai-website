e0771e94a7ea5e64c4df1009de7f2471ea54b3d8
Finn <finnbusse@outlook.de>
2025-01-07 15:49:32 +0100

    added german and english translation

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,18 +1,18 @@
 import i18next from 'i18next';
-import HttpBackend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpBackend from 'i18next-http-backend';
 
 i18next
-    .use(HttpBackend) // Lädt Übersetzungsdateien
-    .use(LanguageDetector) // Erkennt die Sprache des Nutzers
+    .use(HttpBackend) // Übersetzungen von Dateien laden
+    .use(LanguageDetector) // Sprache des Nutzers erkennen
     .init({
         fallbackLng: 'en', // Standardsprache
-        debug: true, // Debugging aktivieren
+        debug: true, // Debug-Modus aktivieren
         interpolation: {
             escapeValue: false, // Kein Escaping für HTML
         },
         backend: {
-            loadPath: '/locales/{{lng}}/translation.json', // Pfad zu den Übersetzungsdateien
+            loadPath: '/locales/{{lng}}/translation.json', // Pfad zu Übersetzungsdateien
         },
     });
 
