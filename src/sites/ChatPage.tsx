acfb2397e519dee32e6eb463909bdaa2054750fb
Finn <finnbusse@outlook.de>
2025-01-07 16:05:04 +0100

    add svg

--- src/sites/ChatPage.tsx
+++ src/sites/ChatPage.tsx
@@ -17,6 +17,8 @@ const EXAMPLE_QUESTION_URL = "https://api.grabbe.site/examples";
 const CHECK_TOKEN_URL = "https://api.grabbe.site/auth/check";
 const EVALUATION_URL = "https://api.grabbe.site/evaluation";
 
+
+
 const ChatPage: React.FC = () => {
     const [messages, setMessages] = useState<{ id: number; text: string; user: "You" | "Bot", evaluation: string }[]>([]);
     const [isBotResponding, setIsBotResponding] = useState<boolean>(false);
@@ -462,22 +464,15 @@ l32 -72 81 -31 c92 -35 178 -57 266 -66 56 -6 72 -2 235 54 96 34 175 61 177
                                     onClick={handleSend}
                                     disabled={isBotResponding || !token || inputText.length > MAX_CHARACTERS}
                                 >
-                                    <svg
+                                    <img
+                                        src="/public/send.svg"
+                                        alt="Send Icon"
                                         width="32"
                                         height="32"
-                                        viewBox="0 0 32 32"
-                                        fill="none"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        <path
-                                            fillRule="evenodd"
-                                            clipRule="evenodd"
-                                            d="M15.1918 8.90615C15.6381 8.45983 16.3618 8.45983 16.8081 8.90615L21.9509 14.049C22.3972 14.4953 22.3972 15.2189 21.9509 15.6652C21.5046 16.1116 20.781 16.1116 20.3347 15.6652L17.1428 12.4734V22.2857C17.1428 22.9169 16.6311 23.4286 15.9999 23.4286C15.3688 23.4286 14.8571 22.9169 14.8571 22.2857V12.4734L11.6652 15.6652C11.2189 16.1116 10.4953 16.1116 10.049 15.6652C9.60265 15.2189 9.60265 14.4953 10.049 14.049L15.1918 8.90615Z"
-                                            fill="currentColor"
-                                        />
-                                    </svg>
+                                    />
                                 </button>
                             </div>
+
                         </div>
 
                         <div className="mt-2 text-center text-gray-600 dark:text-gray-600 text-xs">
