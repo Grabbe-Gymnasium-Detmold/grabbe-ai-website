53c4755a8f3983aab26cccd5cf1ff39eff009118
Finn <finnbusse@outlook.de>
2025-01-07 18:21:14 +0100

    revert ChatPage

--- src/sites/ChatPage.tsx
+++ src/sites/ChatPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, useCallback } from "react";
+import React, {useState, useRef, useEffect, useCallback} from "react";
 import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
@@ -6,10 +6,10 @@ import rehypeHighlight from "rehype-highlight";
 import "katex/dist/katex.css";
 import "highlight.js/styles/github-dark.min.css";
 import Markdown from "react-markdown";
-import { BlueLink } from "@/components/BlueLink.tsx";
+import {BlueLink} from "@/components/BlueLink.tsx";
 import rehypeSemanticBlockquotes from "rehype-semantic-blockquotes";
-import { FaThumbsDown, FaThumbsUp } from "react-icons/fa";
-import { useToast } from "@/components/Toast.tsx";
+import {FaThumbsDown, FaThumbsUp} from "react-icons/fa";
+import {useToast} from "@/components/Toast.tsx";
 
 const API_URL = "https://api.grabbe.site/chat";
 const AUTH_URL = "https://api.grabbe.site/auth";
@@ -34,29 +34,12 @@ const ChatPage: React.FC = () => {
 
     const MAX_CHARACTERS = 150;
 
-    // ========= LANGUAGE =========
-    const [language, setLanguage] = useState<string>('en');
-
-    const translations: { [key: string]: { [key: string]: string } } = {
-        en: {
-            subtitle: "How can I help you today?",
-        },
-        de: {
-            subtitle: "Wobei kann ich dir heute helfen?",
-        }
-    };
-
-    const t = (key: string): string => {
-        return translations[language][key] || key;
-    };
-    // =============================
-
     const authenticate = useCallback(async () => {
         try {
-            const authResponse = await fetch(AUTH_URL, { method: "GET", headers: { "Content-Type": "application/json" } });
+            const authResponse = await fetch(AUTH_URL, {method: "GET", headers: {"Content-Type": "application/json"}});
             if (!authResponse.ok) throw new Error("Authentication failed.");
 
-            const { token: authToken } = await authResponse.json();
+            const {token: authToken} = await authResponse.json();
             localStorage.setItem("session_token", authToken);
             setToken(authToken);
         } catch (error) {
@@ -79,7 +62,7 @@ const ChatPage: React.FC = () => {
             console.error(error);
             return false;
         }
-    }, []);
+    }, [authenticate]);
 
     useEffect(() => {
         const checkToken = async () => {
@@ -97,7 +80,7 @@ const ChatPage: React.FC = () => {
         };
 
         checkToken();
-    }, [authenticate, validateToken]); // Entfernt 'token' aus den Abhängigkeiten, um Endlosschleifen zu vermeiden
+    }, [token, authenticate, validateToken]);
 
     useEffect(() => {
         const fetchExampleQuestions = async () => {
@@ -112,11 +95,11 @@ const ChatPage: React.FC = () => {
 
                 const qResponse = await fetch(EXAMPLE_QUESTION_URL, {
                     method: "GET",
-                    headers: { "Content-Type": "application/json", Authorization: `Bearer ${token}` },
+                    headers: {"Content-Type": "application/json", Authorization: `Bearer ${token}`},
                 });
                 if (!qResponse.ok) throw new Error("Fetching example questions failed.");
 
-                const { questions } = await qResponse.json();
+                const {questions} = await qResponse.json();
                 localStorage.setItem("example_questions", JSON.stringify(questions));
                 setExampleQuestions(shuffleAndSlice(questions, 4));
             } catch (error) {
@@ -150,7 +133,7 @@ const ChatPage: React.FC = () => {
 
         setIsBotResponding(true);
         setShowExampleCards(false);
-        const userMessage = { id: Date.now(), text: inputText.trim(), user: "You" as const, evaluation: "null" };
+        const userMessage = {id: Date.now(), text: inputText.trim(), user: "You" as const, evaluation: "null"};
         setMessages(prev => [...prev, userMessage]);
         setInputText("");
 
@@ -171,7 +154,7 @@ const ChatPage: React.FC = () => {
         }
 
         const botMessageId = Date.now() + 1;
-        setMessages(prev => [...prev, { id: botMessageId, text: "", user: "Bot" as const, evaluation: "null" }]);
+        setMessages(prev => [...prev, {id: botMessageId, text: "", user: "Bot" as const, evaluation: "null"}]);
 
         try {
             const response = await fetch(API_URL, {
@@ -181,7 +164,7 @@ const ChatPage: React.FC = () => {
                     Accept: "text/event-stream",
                     Authorization: `Bearer ${token}`,
                 },
-                body: JSON.stringify({ question: userMessage.text, threadId: currentThreadId }),
+                body: JSON.stringify({question: userMessage.text, threadId: currentThreadId}),
             });
             if (!response.ok) throw new Error("Failed to fetch bot response.");
 
@@ -193,16 +176,17 @@ const ChatPage: React.FC = () => {
             let botMessageText = "";
 
             while (!done) {
-                const { value, done: readerDone } = await reader.read();
+                const {value, done: readerDone} = await reader.read();
                 done = readerDone;
-                const chunk = decoder.decode(value, { stream: true });
-                if (chunk.startsWith('{"done":true,')) {
-                    const { messageId } = JSON.parse(chunk);
-                    setMessages(prev => prev.map(msg => msg.id === botMessageId ? { ...msg, id: messageId } : msg));
-                } else {
+                const chunk = decoder.decode(value, {stream: true});
+                if(chunk.startsWith('{"done":true,')){
+                    const {messageId} = JSON.parse(chunk);
+                    setMessages(prev => prev.map(msg => msg.id === botMessageId ? {...msg, id: messageId} : msg));
+                }else{
                     botMessageText += chunk;
-                    setMessages(prev => prev.map(msg => msg.id === botMessageId ? { ...msg, text: botMessageText } : msg));
+                    setMessages(prev => prev.map(msg => msg.id === botMessageId ? {...msg, text: botMessageText} : msg));
                 }
+
             }
 
         } catch (error: unknown) {
@@ -251,7 +235,7 @@ const ChatPage: React.FC = () => {
 
     const handleEvaluation = async (messageId: number, evaluation: "positive" | "negative") => {
         if (!threadId || !token) return;
-        setMessages(prev => prev.map(msg => msg.id === messageId ? { ...msg, evaluation } : msg));
+        setMessages(prev => prev.map(msg => msg.id === messageId ? {...msg, evaluation} : msg));
         try {
             const response = await fetch(EVALUATION_URL, {
                 method: "POST",
@@ -259,7 +243,7 @@ const ChatPage: React.FC = () => {
                     "Content-Type": "application/json",
                     Authorization: `Bearer ${token}`,
                 },
-                body: JSON.stringify({ threadId, messageId, evaluation }),
+                body: JSON.stringify({threadId, messageId, evaluation}),
             });
 
             if (!response.ok) {
@@ -268,15 +252,9 @@ const ChatPage: React.FC = () => {
             addToast("Danke für deine Bewertung der Nachricht!", "success", 5);
         } catch (error) {
             console.error(error);
-            addToast("Leider gab es einen Fehler beim Senden der Bewertung. Trotzdem danke!", "error", 5);
+            addToast("Leider gab es einen Fehler beim senden der Bewertung. Tortzdem danke!", "success", 5);
         }
     };
-
-    // ========= LANGUAGE =========
-    const handleLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        setLanguage(e.target.value);
-    };
-    // =============================
 
     return (
         <div className="bg-white text-gray-800 flex justify-center items-center min-h-screen dark:bg-gray-800">
@@ -331,26 +309,58 @@ const ChatPage: React.FC = () => {
                                             <path d="M2686 11408 c-137 -18 -269 -116 -329 -245 -29 -63 -31 -77 -32 -173
 0 -181 -45 -122 834 -1100 564 -627 776 -856 821 -887 84 -59 149 -76 269 -71
 83 3 105 8 164 37 229 110 315 388 184 598 -18 28 -373 432 -790 897 -603 672
--770 853-815 881 -92 57 -193 78 -306 63z"/>
-                                            <path d="M9974 11222 c-238 -156 -506 -450 -918 -1005 -218 -295 -424 -589 -850
--416 -250 -110 -277 -128 -340 -222 -54 -82 -73 -153 -68 -259 8 -231 1 -277 63
--339 33 -33 52 -42 127 -60 286 -66 674 -93 1544 -107 372 -6 433 -4 488 10 115
-29 208 107 265 221 38 76 50 136 45 233 -3 75 -9 97 -38 155 -56 105 -158 182
--287 218 -97 27 -1134 42 -1543 23z"/>
+-770 853 -815 881 -92 57 -193 78 -306 63z"/>
+                                            <path d="M9974 11222 c-238 -156 -506 -450 -918 -1005 -218 -295 -424 -589
+-448 -643 -20 -41 -23 -66 -23 -159 0 -103 2 -114 32 -178 42 -89 121 -168
+210 -210 122 -57 293 -49 410 20 69 40 114 91 224 254 104 153 217 311 585
+814 292 400 417 593 451 701 15 47 14 54 -2 105 -51 155 -201 290 -372 334
+-31 8 -62 15 -68 14 -5 0 -42 -22 -81 -47z"/>
+                                            <path d="M895 9350 c-80 -21 -272 -116 -285 -140 -20 -38 -42 -186 -38 -255 8
+-117 54 -185 168 -248 25 -13 99 -57 165 -97 66 -41 206 -115 310 -166 259
+-125 429 -193 1130 -449 110 -40 265 -99 345 -130 172 -67 249 -78 345 -50 86
+24 126 47 194 111 117 108 164 258 127 404 -43 168 -171 270 -491 390 -77 29
+-207 79 -290 112 -219 87 -589 222 -1025 374 -212 74 -402 141 -423 149 -50
+20 -147 18 -232 -5z"/>
+                                            <path d="M6130 8858 c-905 -40 -1743 -489 -2284 -1223 -395 -534 -592 -1196
+-556 -1865 57 -1044 658 -1963 1604 -2451 328 -168 681 -275 1061 -320 138
+-17 527 -17 660 0 485 59 916 211 1301 457 1062 679 1576 1920 1294 3130 -226
+968 -960 1774 -1921 2109 -376 130 -743 182 -1159 163z"/>
                                             <path d="M11915 8759 c-224 -12 -444 -54 -1005 -194 -548 -137 -868 -224 -917
 -248 -190 -97 -280 -327 -204 -526 44 -115 160 -223 279 -257 109 -32 159 -25
 707 100 904 207 1140 273 1372 386 132 65 164 96 263 263 36 61 35 152 -3 227
 -25 49 -103 154 -173 233 -27 31 -40 31 -319 16z"/>
-                                            <path d="M895 9350 c-80 -21 -272 -116 -285 -140 -20 -38 -42 -186 -38 -255 8
--117 54 -185 168 -248 25 -13 99 -57 165 -97 66 -41 206 -115 310 -166 116 -68
-265 -156 333 -194 376 -213 637 -293 828 -254 91 18 186 61 237 105 83 73 126
-234 106 392 -29 231 -190 336 -1284 836 -293 135 -402 175 -1159 434 -203 93
--384 173 -408 175 -33 3 -68 -2 -85 -10z"/>
+                                            <path d="M12255 6499 c-61 -19 -442 -29 -1280 -35 -923 -6 -920 -6 -1024 -58
+-153 -78 -247 -263 -221 -436 22 -140 113 -259 247 -323 l68 -32 595 -3 c595
+-3 1384 10 1638 29 210 15 283 39 385 129 115 101 167 318 110 463 -47 122
+-151 220 -270 257 -70 21 -193 26 -248 9z"/>
+                                            <path d="M670 6219 c-277 -13 -516 -50 -601 -95 -52 -26 -56 -44 -63 -254 -8
+-231 1 -277 63 -339 33 -33 52 -42 127 -60 286 -66 674 -93 1544 -107 372 -6
+433 -4 488 10 115 29 208 107 265 221 38 76 50 136 45 233 -3 75 -9 96 -38
+150 -56 105 -158 182 -287 218 -97 27 -1134 42 -1543 23z"/>
+                                            <path d="M2630 4589 c-25 -5 -373 -171 -800 -383 -415 -206 -798 -393 -850
+-416 -250 -110 -277 -128 -340 -222 -54 -82 -73 -153 -68 -259 3 -73 9 -97 38
+-155 44 -90 107 -152 204 -200 72 -35 87 -39 175 -42 77 -3 108 0 156 16 94
+32 394 176 1130 542 724 360 744 372 811 476 49 76 67 151 62 255 -3 74 -9 97
+-37 153 -90 176 -287 273 -481 235z"/>
+                                            <path d="M9960 4343 c-56 -11 -153 -65 -205 -114 -99 -94 -148 -237 -125 -368
+27 -155 105 -247 296 -350 65 -36 214 -121 329 -189 116 -68 265 -156 333
+-194 376 -213 637 -293 828 -254 91 18 186 61 237 105 83 73 126 234 106 392
+-29 231 -190 336 -1284 836 -293 135 -319 143 -414 142 -42 -1 -87 -4 -101 -6z"/>
+                                            <path d="M3851 2964 c-102 -27 -142 -59 -453 -362 -622 -606 -1082 -1041
+-1191 -1125 -70 -54 -111 -105 -146 -177 -24 -50 -26 -66 -26 -175 l1 -120 54
+-84 c48 -74 64 -90 140 -141 78 -51 95 -57 171 -69 189 -27 274 17 579 297
+323 297 1284 1228 1335 1294 59 76 79 140 79 254 1 96 0 103 -37 176 -40 82
+-116 162 -188 199 -94 47 -217 60 -318 33z"/>
+                                            <path d="M8595 2730 c-106 -17 -186 -61 -262 -143 -47 -51 -68 -88 -89 -156
+-19 -64 -18 -187 3 -252 26 -81 475 -750 725 -1081 279 -370 414 -492 595
+-534 218 -51 449 83 515 299 46 153 0 320 -117 423 -112 98 -167 158 -283 312
+-158 208 -309 424 -503 714 -88 131 -176 257 -196 279 -97 107 -249 161 -388
+139z"/>
                                             <path d="M6375 2434 c-123 -32 -211 -91 -273 -183 -64 -96 -69 -128 -76 -461
--8 -162 -13 -412 -21 -555 -17 -303 -19 -684 -5 -805 14 -119 29 -174 66 -256
+-3 -162 -13 -412 -21 -555 -17 -303 -19 -684 -5 -805 14 -119 29 -174 66 -256
 l32 -72 81 -31 c92 -35 178 -57 266 -66 56 -6 72 -2 235 54 96 34 175 61 177
 61 5 0 24 80 38 160 23 140 35 339 35 614 0 320 -27 1121 -40 1196 -24 132
--119 252 -226 312 -56 26 -80 31 -158 34 -50 2 -100 1 -111 -2z"/>
+-119 252 -246 312 -56 26 -80 31 -158 34 -50 2 -100 1 -111 -2z"/>
                                         </g>
                                     </svg>
 
@@ -358,18 +368,9 @@ l32 -72 81 -31 c92 -35 178 -57 266 -66 56 -6 72 -2 235 54 96 34 175 61 177
                             </div>
                         </button>
 
-                        {/* ========= LANGUAGE DROPDOWN ========= */}
-                        <div className="absolute top-4 left-4">
-                            <select value={language} onChange={handleLanguageChange} className="bg-gray-200 dark:bg-gray-600 text-gray-800 dark:text-white rounded px-2 py-1">
-                                <option value="en">English</option>
-                                <option value="de">Deutsch</option>
-                            </select>
-                        </div>
-                        {/* ===================================== */}
-
-                        <div className="title text-2xl font-semibold mb-4">{t("title")}</div>
+                        <div className="title text-2xl font-semibold mb-4">GrabbeAI</div>
                         <div className="subtitle text-base text-gray-600 mb-10">
-                            {t("subtitle")}
+                            Wobei kann ich dir heute helfen?
                         </div>
 
                         {showExampleCards && (
@@ -383,8 +384,8 @@ l32 -72 81 -31 c92 -35 178 -57 266 -66 56 -6 72 -2 235 54 96 34 175 61 177
                                             handleSend();
                                         }}
                                     >
-                        {question}
-                    </span>
+                {question}
+            </span>
                                 ))}
                             </div>
 
@@ -412,23 +413,23 @@ l32 -72 81 -31 c92 -35 178 -57 266 -66 56 -6 72 -2 235 54 96 34 175 61 177
                                     {!isBotResponding && (
                                         <div
                                             className="absolute transform translate-x-4 translate-y-4 opacity-0 group-hover:opacity-100 flex space-x-2">
-                                            {msg.evaluation === "null" && (
+                                            {msg.evaluation == "null" && (
                                                 <>
                                                     <FaThumbsUp
                                                         onClick={() => handleEvaluation(msg.id, "positive")}
-                                                        className="text-lg text-green-500 cursor-pointer hover:scale-110 transition-transform duration-300" />
+                                                        className="text-lg text-green-500 cursor-pointer hover:scale-110 transition-transform duration-300"/>
                                                     <FaThumbsDown
                                                         onClick={() => handleEvaluation(msg.id, "negative")}
-                                                        className="text-lg text-red-500 cursor-pointer hover:scale-110 transition-transform duration-300" />
+                                                        className="text-lg text-red-500 cursor-pointer hover:scale-110 transition-transform duration-300"/>
                                                 </>
                                             )}
-                                            {msg.evaluation === "positive" && (
+                                            {msg.evaluation == "positive" && (
                                                 <FaThumbsUp
-                                                    className="text-lg text-green-500 cursor-default hover:scale-100 transition-none" />
+                                                    className="text-lg text-green-500 cursor-default hover:scale-100 transition-none"/>
                                             )}
-                                            {msg.evaluation === "negative" && (
+                                            {msg.evaluation == "negative" && (
                                                 <FaThumbsDown
-                                                    className="text-lg text-red-500 cursor-default hover:scale-100 transition-none" />
+                                                    className="text-lg text-red-500 cursor-default hover:scale-100 transition-none"/>
                                             )}
                                         </div>
                                     )}
@@ -455,7 +456,7 @@ l32 -72 81 -31 c92 -35 178 -57 266 -66 56 -6 72 -2 235 54 96 34 175 61 177
                                 <input
                                     ref={inputRef}
                                     type="text"
-                                    placeholder={t("input_placeholder")}
+                                    placeholder="Schreibe GrabbeAI..."
                                     className="flex-1 bg-transparent outline-none text-base px-2 rounded-full dark:text-white dark:placeholder-white"
                                     value={inputText}
                                     onChange={handleInputChange}
