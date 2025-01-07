ef9efdda7b7dc689d16995a38161a957ce496714
Finn Busse <110921874+finnbusse@users.noreply.github.com>
2025-01-07 11:51:09 +0100

    speed optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"), // Alias für den `src`-Ordner
     },
   },
-})
+  build: {
+    sourcemap: true, // Aktiviert Source Maps für Debugging
+    minify: "terser", // Nutzt Terser für eine bessere Minifizierung
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"], // Separiere Vendor-Bibliotheken wie React
+        },
+      },
+    },
+  },
+});
