754a139da430d512911b7f100b73eeacf041053a
Finn <finnbusse@outlook.de>
2025-01-21 18:03:04 +0100

    adjusting "Neu" badge

--- src/sites/ChatPage.tsx
+++ src/sites/ChatPage.tsx
@@ -407,8 +407,8 @@ const ChatPage: React.FC = () => {
       focus:outline-none
     "
                 >
-                    Frage vorschlagen<span
-                    className="bg-blue-100 text-blue-800 text-2xl font-semibold me-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-2">NEU</span>
+                    Frage vorschlagen <span
+                    className="bg-blue-100 text-blue-800 text-sm font-semibold me-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-2">NEU</span>
                 </button>
             </div>
 
